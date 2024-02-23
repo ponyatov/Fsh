9e912d43b6a28501dc755a12b5bb0b6e24a76799
Dmitry Ponyatov <dponyatov@gmail.com>
2024-02-23 11:37:48 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- lib/compiler.fs
+++ lib/compiler.fs
@@ -27,3 +27,21 @@ let rec eval (e: expr) : int =
 eval i17 // val it: int = 17
 eval minus34 // -1
 eval addmul // 73
+
+// expressions consisting only of
+// integer constants and
+// twoargument (dyadic) operators such as (+) and (*).
+type expr =
+    // integer constants are represented by constructor
+    | CstI of int
+    // binary operator applications
+    | Prim of string * expr * expr
+    // variable
+    | Var of string
+
+Var "X" // x
+Prim("+", CstI 3, Var "a") // 3 + a
+
+// environment
+let env = [ ("a", 3); ("c", 78); ("baf", 666); ("b", 111) ]
+let empty_env = []
