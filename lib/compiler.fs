dcea07a1317147f2be6d3f1f7f420274ade66fad
Dmitry Ponyatov <dponyatov@gmail.com>
2024-02-23 11:21:24 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- lib/compiler.fs
+++ lib/compiler.fs
@@ -7,5 +7,23 @@
 type expr =
     // integer constants are represented by constructor
     | CstI of int
-    //
+    // binary operator applications
     | Prim of string * expr * expr
+
+// expr samples
+let i17 = CstI 17 // 17
+let minus34 = Prim("-", CstI 3, CstI 4) // 3 - 4
+let addmul = Prim("+", Prim("*", CstI 7, CstI 9), CstI 10) // 7 * 9 + 10
+
+// expr evaluation function
+let rec eval (e: expr) : int =
+    match e with
+    | CstI i -> i
+    | Prim("+", e1, e2) -> eval e1 + eval e2
+    | Prim("-", e1, e2) -> eval e1 - eval e2
+    | Prim("*", e1, e2) -> eval e1 * eval e2
+    | Prim _ -> failwith "unknown primitive"
+
+eval i17 // val it: int = 17
+eval minus34 // -1
+eval addmul // 73
