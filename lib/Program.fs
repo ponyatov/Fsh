4c0ebc9333a3d50d9a60b89a64875e5f56b82d46
Dmitry Ponyatov <dponyatov@cad.local>
2024-03-01 12:25:09 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@cad.local>

--- lib/Program.fs
+++ lib/Program.fs
@@ -2,3 +2,16 @@
 
 // For more information see https://aka.ms/fsharp-console-apps
 printfn "Hello from F#"
+
+let x = [ 1..10..110 ]
+
+(fun x ->
+    [| for i in 1..x do
+           yield i |])
+    11
+
+List.zip [ 0..9 ] [ 9..-1..0 ]
+
+let a, b = List.partition (fun x -> x % 2 = 0) [ 0..11 ]
+
+List.map (fun x -> pown x 2) [ 0..9 ]
