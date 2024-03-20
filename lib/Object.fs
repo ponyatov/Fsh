6805adac34092d04adbda22787875d527c6abd10
Dmitry Ponyatov <dponyatov@gmail.com>
2024-03-20 23:55:43 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- lib/Object.fs
+++ lib/Object.fs
@@ -1,1 +1,11 @@
 ﻿module Object
+
+type Object =
+    val value : string
+    new() = Object("")
+    new(v:string) = { value=v }
+    override this.ToString() =
+        "<object:" + this.value + ">"
+
+new Object("sa")
+new Object()
