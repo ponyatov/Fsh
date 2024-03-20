ffcb4289ad10ae613981aff9f9c41993501b9ffb
Dmitry Ponyatov <dponyatov@gmail.com>
2024-03-21 00:19:18 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- lib/Object.fs
+++ lib/Object.fs
@@ -1,14 +1,20 @@
 ﻿module Object
 
-type Object(value:string) =
+type Object(value: string) =
     let nest: Object list = []
+    new() = new Object("")
+
     override this.ToString() =
         let ret = new System.Text.StringBuilder()
-        ret.Append("<object:")
-        ret.Append(value)
-        ret.Append("> [")
-        for i in nest do ret.Append(i.ToString()+" ") |>ignore
-        ret.Append("]")
+        ret.Append("<object:") |> ignore
+        ret.Append(value) |> ignore
+        ret.Append("> [") |> ignore
+
+        for i in nest do
+            ret.Append(i.ToString() + " ") |> ignore
+
+        ret.Append("]") |> ignore
         ret.ToString()
 
 Object "123"
+Object()
